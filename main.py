db4ba2d0f5f2bf81595a59f43d6ff1ba3038d2d5
Amokke <24.02.1985@mail.ru>
2024-03-12 15:47:54 +0700

    add cloth

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/cloth/')
+def cloth():
+    _shoes = [
+        {
+            "name": "Шапка-ушанка",
+            "price": '4500 Р',
+            "description": ' Зимняя меховая, суконная или комбинированная шапка (первоначально — мужская), широко распространённый головной убор в России.',
+        },
+        {
+            "name": "Шляпа",
+            "price": '2500 Р',
+            "description": 'головной убор, состоящий из двух частей ',
+        },
+    ]
+    return render_template('cloth.html', content=_cloth)
+
+
 @app.route('/shoes/')
 def shoes():
     _shoes = [
